bafac64dcf875e9b43300985fd383fda6b755c01
neelsh <neelsh@umich.edu>
2020-11-17 16:08:12 -0500

    Updates to Prox

--- LinearMap/Prox.py
+++ LinearMap/Prox.py
@@ -1,55 +1,142 @@
 import torch
 
 class Prox():
-   #TODO Figure out exact syntax for running operations on GPU (cuda) or CPU with default being GPU
+    r"""
+    Proximal operator base class
+
+    Prox is currently supported to be called on a torch.Tensor
+
+    .. math::
+        \arg \min_x \frac{1}{2} \| x - v \|_2^2 + f(x)
+
+    Args:
+        device (None or torch.device): device of output tensor, default on same device as input
+    """
 
     def __init__(self, device = None):
-        self._device = device
+        self.device = device
         #sigpy has size/shape input parameter, but I don't see why we would need it?: Maybe for now we do not need it
-        pass
+        
 
-    
-    def __call__(self, input): 
+    def __call__(self, v): 
         #sigpy also has alpha value, maybe add that here after implementing basic functionality
-        return self._apply(input)
+        return self._apply(v)
 
 class L1Regularizer(Prox):
-    # Please add the latex expression of what is happening here:
-    def __init__(self, Lambda, device):
+    r"""
+    Proximal operator for L1 regularizer, using soft thresholding
+
+    .. math::
+        \arg \min_x \frac{1}{2} \| x - v \|_2^2 + \lambda \| x \|_1
+
+    Args:
+        Lambda (float): regularization parameter.
+        device (None or torch.device): device of output tensor, default on same device as input
+    """
+    
+    def __init__(self, Lambda, device = None):
         super().__init__(device)
         self.Lambda = float(Lambda)
 
-    def _apply(self, input):
+    def _apply(self, v):
         # Closed form solution from
         # https://archive.siam.org/books/mo25/mo25_ch6.pdf
 
-        with self._device: # This is a simple function, input and output are on the same device. Maybe we do not need this
-            abs = torch.abs(input, device=self._device)
-            sign = input.div(abs)
-            sign[sign == float("Inf")] = 0
-            # I suggest using torch.sign here. If Inf exist there should be something wrong. We do not expect operation to handle this
+        #utilize torch.nn.Softshrink
+        thresh = torch.nn.Softshrink(self.Lambda)
 
-            abs = abs - self.Lambda
-            abs[abs < 0] = 0 # this operation seems to be not differentiable to me. You can double check it.
-            # Suggestion: check https://pytorch.org/docs/stable/_modules/torch/nn/modules/activation.html#Softshrink for how they are dealing with the gradient
+        x = thresh(v)
 
-            return torch.abs * sign
+        if self.device is not None:
+            x = x.to(self.device)
+
+        return x
+
 
 
 
 class L2Regularizer(Prox):
-    def __init__(self, Lambda, device):
+    r"""
+    Proximal operator for L2 regularizer
 
+    .. math::
+        \arg \min_x \frac{1}{2} \| x - v \|_2^2 + \lambda \| x \|_2
 
+    Args:
+        Lambda (float): regularization parameter.
+        device (None or torch.device): device of output tensor, default on same device as input
+    """
+    def __init__(self, Lambda, device=None):
         super().__init__(device)
-        self.
-        Lambda = float(Lambda)
+        self.Lambda = float(Lambda)
 
-    def _apply(self, input):
+    def _apply(self, v):
         # Closed form solution from
         # https://archive.siam.org/books/mo25/mo25_ch6.pdf
         # Again, pls add the math expression
-        with self._device:
-            scale = 1 - self.lambda / max(lambda, torch.linalg.norm(input))
-            return scale * input
+        
+        scale = 1 - self.Lambda / torch.max(v, torch.linalg.norm(v))
 
+        x = torch.mul(scale, v)
+
+        if self.device is not None:
+            x = x.to(self.device)
+
+        return x
+
+class SqauredL2Regularizer(Prox):
+
+    r"""
+    Proximal operator for Squared L2 regularizer
+
+    .. math::
+        \arg \min_x \frac{1}{2} \| x - v \|_2^2 + \lambda \| x \|_2^2
+
+    Args:
+        Lambda (float): regularization parameter.
+        device (None or torch.device): device of output tensor, default on same device as input
+    """
+
+    def __init__(self, Lambda, device=None):
+
+        super().__init__(device)
+        self.Lambda = float(Lambda)
+    
+    def _apply(self, v):
+
+        x = torch.div(v, 1 + 2*self.Lambda)
+
+        if self.device is not None:
+            x = x.to(self.device)
+        
+        return x
+
+class BoxConstraint(Prox):
+
+    r"""
+    Proximal operator for Box Constraint
+
+    .. math::
+        \arg \min_{x \in [lower, upper]} \frac{1}{2} \| x - v \|_2^2
+
+    Args:
+        Lambda (float): regularization parameter.
+        device (None or torch.device): device of output tensor, default on same device as input
+    """
+
+    def __init__(self, lower, upper, device=None):
+        
+        super().__init__(device)
+        self.l = lower
+        self.u = upper
+        self.Lambda = float(Lambda)
+    
+    def _apply(self, v):
+
+        x = torch.clamp(v, self.l, self.u)
+
+        if self.device is not None:
+            x = x.to(self.device)
+        
+        return x
+        
