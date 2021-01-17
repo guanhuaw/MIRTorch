d4c8bc0b3009691054fa453e55f6c2a69beabed1
Guanhua <guanhuaw@umich.edu>
2021-01-17 09:54:09 -0500

    fix linear, addmri example

--- linear/linearmaps.py
+++ linear/linearmaps.py
@@ -1,66 +1,69 @@
 """
-Linear Operator implementations.
+Linear Operator implementations, based on SigPy:
+https://github.com/mikgroup/sigpy
 """
 import torch
 import abc
 import os
 import numpy as np
 
-# To Do: frame operator
-
+# To Do: frame operators, extended assignments, unary operators
 '''
- Recommendation for linear operation:
- class forward(torch.autograd.Function):
-    @staticmethod
-    def forward(ctx, data_in):
-        return forward_func(data_in)
-    @staticmethod
-    def backward(ctx, grad_data_in):
-        return adjoint_func(grad_data_in)
-forward_op = forward.apply # This may look wired to you. But the torch.autograd. Function requires .apply
-
-class adjoint(torch.autograd.Function):
-    @staticmethod
-    def forward(ctx, data_in):
-        return forward_func(data_in)
-    @staticmethod
-    def backward(ctx, grad_data_in):
-        return adjoint_func(grad_data_in)
-adjoint_op = adjoint.apply
+    Recommendation for linear operation:
+     class forward(torch.autograd.Function):
+        @staticmethod
+        def forward(ctx, data_in):
+            return forward_func(data_in)
+        @staticmethod
+        def backward(ctx, grad_data_in):
+            return adjoint_func(grad_data_in)
+     forward_op = forward.apply
+    
+     class adjoint(torch.autograd.Function):
+        @staticmethod
+        def forward(ctx, data_in):
+            return forward_func(data_in)
+        @staticmethod
+        def backward(ctx, grad_data_in):
+            return adjoint_func(grad_data_in)
+     adjoint_op = adjoint.apply
 '''
 
 
 def check_device(x, y):
-    assert x.device == y.device, "tensors should be on the same device"
+    assert x.device == y.device, "Tensors should be on the same device"
 
 
 class LinearMap():
     '''
         We followed the idea of Sigpy rather than ModOpt:
-        Each son class (like FFT, Wavelet ...) defines it own _apply and _apply_adjoint
-        This approach lacks the versatility of define new linear operator on the run, but is easier to implemented
+        Each instance (like FFT, Wavelet ...) defines it own _apply and _apply_adjoint
+        This approach lacks the versatility of define new linear operator on the run,
+        but is easier to implement.
     '''
 
     def __init__(self, size_in, size_out, device='cuda:0'):
         '''
-        For initilization, this can be provided:
-        size_in: the size of the input of the linear map (a list)
-        size_out: the size of the output of the linear map (a list)
+            Initilization requires:
+            size_in: the size of the input of the linear map (a list)
+            size_out: the size of the output of the linear map (a list)
         '''
-        self.size_in = size_in  # size_in: input data dimention
-        self.size_out = size_out  # size_out: output data dimention
-        self.device = device
+        self.size_in = size_in  # size_in: input data dimension
+        self.size_out = size_out  # size_out: output data dimension
+        self.device = device      # some linear operators do not depend on devices, like FFT.
+        self.property = None      # properties like unitary, Toeplitz...
 
     def __repr__(self):
-        # Name of the linear operator
+        # name of the linear operator
         pass
 
     def __call__(self, x):
-        # For a instance of LinearOP class, apply it by A(x). Equal to A*x
-        return self._apply(x)
+        # for a instance A, we can apply it by calling A(x). Equal to A*x
+        return self.apply(x)
 
     def _apply(self, x):
-        # Worth noting that the function here should be differentiable, for example, composed of native torch functions,
+        # worth noting that the function here should be differentiable,
+        # for example, composed of native torch functions,
         # or torch.autograd.Function, or nn.module
         raise NotImplementedError
 
@@ -68,11 +71,13 @@ class LinearMap():
         raise NotImplementedError
 
     def apply(self, x):
-        assert x.shape == self.size_in, "input size and linear op size does not match"
+        assert list(x.shape) == list(self.size_in), "Shape of input data and forward linear op do not match!"
         return self._apply(x)
+
     def adjoint(self, x):
-        assert x.shape == self.size_out, "input size and adjoint op size does not match"
+        assert list(x.shape) == list(self.size_out), "Shape of input data and adjoint linear op do not match!"
         return self._apply_adjoint(x)
+
     @property
     def H(self):
         return ConjTranspose(self)
@@ -105,8 +110,9 @@ class LinearMap():
     def __neg__(self):
         return -1 * self
 
-    def __matmul__(self,other):
+    def __matmul__(self, other):
         pass
+
 
 class Add(LinearMap):
     '''
@@ -161,6 +167,7 @@ class Matmul(LinearMap):
         super().__init__(self.B.size_in, self.A.size_out)
 
     def _apply(self, x):
+        # TODO: add frame operator
         return self.A(self.B(x))
 
     def _apply_adjoint(self, x):
@@ -177,4 +184,3 @@ class ConjTranspose(LinearMap):
 
     def _apply_adjoint(self, x):
         return self.A.apply(x)
-
