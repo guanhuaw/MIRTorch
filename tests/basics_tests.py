df022b603a934514b64fc923e2c0927c25244b7a
Keyue Zhu <zhukeyue@umich.edu>
2021-03-03 09:50:53 -0500

    Update testcase, using utils now

--- tests/basics_tests.py
+++ tests/basics_tests.py
@@ -28,8 +28,12 @@ class TestBasic(unittest.TestCase):
         conv = basics.Convolve1d(x.shape, weight, device=device)
         out = conv.apply(x)
 
-        exp = F.conv1d(x, weight)
-        assert(torch.allclose(out, exp, rtol=1e-3))
+        # exp = F.conv1d(x, weight)
+        x = x.permute(0,2,1).detach().cpu().numpy()
+        weight = weight.permute(2,1,0).detach().cpu().numpy()
+        exp = utils.conv1D(x, weight, stride=1, pad=0, dilation=0)
+        exp = torch.from_numpy(exp).to(device).permute(0,2,1)
+        assert(torch.allclose(out, exp, rtol=1.5e-3))
 
     def test_conv2d_apply_simple(self):
         x = torch.randn(1, 4, 5, 5)
@@ -40,7 +44,11 @@ class TestBasic(unittest.TestCase):
         conv = basics.Convolve2d(x.shape, weight, device=device)
         out = conv.apply(x)
 
-        exp = F.conv2d(x, weight)
+        # exp = F.conv2d(x, weight)
+        x = x.permute(0,2,3,1).detach().cpu().numpy()
+        weight = weight.permute(2,3,1,0).detach().cpu().numpy()
+        exp = utils.conv2D(x, weight, stride=1, pad=0, dilation=0)
+        exp = torch.from_numpy(exp).to(device).permute(0,3,1,2)
         assert(torch.allclose(out, exp, rtol=1e-3))
 
     def test_conv3d_apply_simple(self):
@@ -136,15 +144,15 @@ class TestBasic(unittest.TestCase):
     def test_conv1d_adjoint_hard(self):
         x = torch.randn(20, 16, 50)
         weight = torch.randn(33, 16, 3)
-        bias = torch.randn(33)
+        bias = torch.randn(16)
         device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         x, weight, bias = x.to(device), weight.to(device), bias.to(device)
 
         Ax = F.conv1d(x, weight, stride=2, padding=1, dilation=2)
-        conv = basics.Convolve1d(x.shape, weight, stride=2, padding=1, dilation=2, device=device)
+        conv = basics.Convolve1d(x.shape, weight, bias=bias, stride=2, padding=1, dilation=2, device=device)
         out = conv.adjoint(Ax)
 
-        exp = F.conv_transpose1d(Ax, weight, stride=2, padding=1, dilation=2)
+        exp = F.conv_transpose1d(Ax, weight, bias=bias, stride=2, padding=1, dilation=2)
         assert(torch.allclose(out, exp, rtol=1e-3))
 
 
@@ -152,17 +160,3 @@ if __name__ == '__main__':
     # if torch.cuda.is_available():
     #     print(torch.cuda.get_device_name(0))
     unittest.main()
-
-    # x = torch.randn(20, 16, 50)
-    # weight = torch.randn(33, 16, 3)
-    # bias = torch.randn(33)
-    # device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    # x, weight, bias = x.to(device), weight.to(device), bias.to(device)
-
-    # Ax = F.conv1d(x, weight, bias=bias, stride=2, padding=1, dilation=2)
-    # conv = basics.Convolve1d(x.shape, weight, bias=bias, stride=2, padding=1, dilation=2, device=device)
-    # print(conv.size_in, conv.size_out)
-    # print(Ax.shape)
-    # out = conv.adjoint(Ax)
-
-    # exp = F.conv_transpose1d(Ax, weight, bias=bias, stride=2, padding=1, dilation=2)
