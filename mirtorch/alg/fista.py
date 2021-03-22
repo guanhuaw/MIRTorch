05b8f08b861c25c56c453b9f413b4a305b5b27d0
neelsh <neelsh@umich.edu>
2021-03-21 22:32:43 -0400

    get very very toy example in test_fista.ipynb and clean up fista.py, now test fista implementation on more realistic examples

--- mirtorch/alg/fista.py
+++ mirtorch/alg/fista.py
@@ -2,31 +2,41 @@
 See test_fista.ipynb to see more up to date implementation
 '''
 class FISTA():
-    def __init__(self, max_iter, step, fval, grad, prox, momentum = 1, restart = False):
+    r'''
+    Fast Iterative Soft Thresholding Algorithm (FISTA)
+
+    .. math::
+        \arg \min_x f(x) + g(x)
+     where grad(f(x)) is L-Lipschitz continuous and g is proximal operator
+    Args:
+        max_iter (int): number of iterations to run
+        grad (LinearMap): gradient of f, scaled by a factor of 1/L
+        prox (Prox): proximal operator g
+        restart (Union[...]): restart strategy, not yet implemented
+    '''
+    def __init__(self, max_iter, grad, prox, restart = False):
         self.max_iter = max_iter
-        self.step = step
-        self.fval = fval
+        self.step = 1
         self.grad = grad
         self.prox = prox
-        self.momentum = momentum
         self.restart = restart
     '''
     Experimenting different implementation
     '''
-    def _update(self, iter):
+    def _update(self):
         step_prev = self.step
         self.step = (1 + np.sqrt(1 + 4*step_prev*step_prev))/2
         self.momentum = (step_prev-1)/self.step
 
     def run_alg(self, x0):
         x_curr = x0
-        z_curr = x0 
+        z_curr = x0
         for i in range(max_iter):
+            #update momentum parameters
+            self._update()
             x_prev = x_curr
             z_prev = z_curr
             #compute new z_k and x_k
-            z_curr = self.prox(x_prev - self.grad(x_prev)) 
+            z_curr = self.prox(x_prev - self.grad(x_prev))
             x_curr = z_curr + self.momentum * (z_curr - z_prev)
-            #update momentum value for next iteration
-            self._update()
         return x_curr
