178bed78be8eac4181cc6cc0e3c271b8b8651619
neelsh <neelsh@umich.edu>
2021-09-27 16:59:28 -0400

    Implement prox.Conj, begin prox.Stack, add typing hints in prox

--- mirtorch/prox/prox.py
+++ mirtorch/prox/prox.py
@@ -3,8 +3,12 @@ Proximal operators, including soft-thresholding, box-constraint and L2 norm.
 2021-02. Neel Shah and Guanhua Wang, University of Michigan
 """
 
+from mirtorch.linear import LinearMap
 import torch
+from typing import Union
 
+
+FloatLike = Union[float, torch.FloatTensor]
 
 class Prox:
     r"""
@@ -21,7 +25,7 @@ class Prox:
         TODO: manually check if it is unitary or diagonal
     """
 
-    def __init__(self, T = None, P = None):
+    def __init__(self, T: LinearMap = None, P: LinearMap = None):
         # if T is not None:
         #     assert('unitary' in T.property)
         self.T = T
@@ -31,10 +35,10 @@ class Prox:
         self.P = P
 
 
-    def _apply(self, v, alpha):
+    def _apply(self, v: torch.Tensor, alpha: FloatLike):
         raise NotImplementedError
 
-    def __call__(self, v, alpha):
+    def __call__(self, v: torch.Tensor, alpha: FloatLike):
         #sigpy also has alpha value, maybe add that here after implementing basic functionality
 
         if self.T is not None:
@@ -60,7 +64,7 @@ class L1Regularizer(Prox):
     Proximal operator for L1 regularizer, using soft thresholding
 
     .. math::
-        \arg \min_x \frac{1}{2} \| x - v \|_2^2 + \lambda \| PTx \|_1
+        \arg \min_x \frac{1}{2} \| x - v \|_2^2 + \alpha \lambda \| PTx \|_1
 
     Args:
         Lambda (float): regularization parameter.
@@ -69,7 +73,7 @@ class L1Regularizer(Prox):
     """
 
 
-    def __init__(self, Lambda, T = None, P = None):
+    def __init__(self, Lambda, T: LinearMap = None, P: LinearMap = None):
         super().__init__(T, P)
         self.Lambda = float(Lambda)
         assert self.Lambda > 0, "alpha should be greater than 0"
@@ -85,7 +89,7 @@ class L1Regularizer(Prox):
         out += mask2.float() * lambd + mask2.float() * x
         return out
 
-    def _apply(self, v, alpha):
+    def _apply(self, v: torch.Tensor, alpha: FloatLike):
         assert alpha > 0, "alpha should be greater than 0"
         if type(self.Lambda) is not torch.Tensor and type(alpha) is not torch.Tensor:
             # The softshrink function do not support tensor as Lambda.
@@ -101,7 +105,7 @@ class L0Regularizer(Prox):
     Proximal operator for L0 regularizer, using hard thresholding
 
     .. math::
-        \arg \min_x \frac{1}{2} \| x - v \|_2^2 + \lambda \| PTx \|_0
+        \arg \min_x \frac{1}{2} \| x - v \|_2^2 + \alpha \lambda \| PTx \|_0
 
     Args:
         Lambda (float): regularization parameter.
@@ -110,7 +114,7 @@ class L0Regularizer(Prox):
     """
 
 
-    def __init__(self, Lambda, T = None, P = None):
+    def __init__(self, Lambda, T: LinearMap = None, P: LinearMap = None):
         super().__init__(T, P)
         self.Lambda = float(Lambda)
         if P is not None:
@@ -125,7 +129,7 @@ class L0Regularizer(Prox):
         out += mask2.float() * x
         return out
 
-    def _apply(self, v, alpha):
+    def _apply(self, v: torch.Tensor, alpha: FloatLike):
         assert alpha > 0, "alpha should be greater than 0"
         if type(self.Lambda) is not torch.Tensor and type(alpha) is not torch.Tensor:
             # The hardthreshold function do not support tensor as Lambda.
@@ -141,20 +145,20 @@ class L2Regularizer(Prox):
     Proximal operator for L2 regularizer
 
     .. math::
-        \arg \min_x \frac{1}{2} \| x - v \|_2^2 + \lambda \| Tx \|_2
+        \arg \min_x \frac{1}{2} \| x - v \|_2^2 + \alpha \lambda \| Tx \|_2
 
     Args:
         Lambda (float): regularization parameter.
         T (LinearMap): optional, unitary LinearMap
     """
-    def __init__(self, Lambda, T = None, P = None):
+    def __init__(self, Lambda, T: LinearMap = None, P: LinearMap = None):
         super().__init__(T, P)
         self.Lambda = float(Lambda)
         if P is not None:
             self.Lambda = P(Lambda*torch.ones(P.size_in))
 
     
-    def _apply(self, v, alpha):
+    def _apply(self, v: torch.Tensor, alpha: FloatLike):
         # Closed form solution from
         # https://archive.siam.org/books/mo25/mo25_ch6.pdf
         if self.P is None:
@@ -169,20 +173,20 @@ class SquaredL2Regularizer(Prox):
     Proximal operator for Squared L2 regularizer
 
     .. math::
-        \arg \min_x \frac{1}{2} \| x - v \|_2^2 + \lambda \| Tx \|_2^2
+        \arg \min_x \frac{1}{2} \| x - v \|_2^2 + \alpha \lambda \| Tx \|_2^2
 
     Args:
         Lambda (float): regularization parameter.
         T (LinearMap): optional, unitary LinearMap
     """
-    def __init__(self, Lambda, T = None, P = None):
+    def __init__(self, Lambda, T: LinearMap = None, P: LinearMap = None):
         super().__init__(T, P)
         self.Lambda = float(Lambda)
         if P is not None:
             self.Lambda = P(Lambda*torch.ones(P.size_in))
 
 
-    def _apply(self, v, alpha):
+    def _apply(self, v: torch.Tensor, alpha: FloatLike):
         if self.P is None:
             x = torch.div(v, 1 + 2*self.Lambda*alpha)
         else:
@@ -203,14 +207,14 @@ class BoxConstraint(Prox):
         T (LinearMap): optional, unitary LinearMap
     """
 
-    def __init__(self, Lambda, lower, upper, T = None, P = None):
-        super().__init__(T)
+    def __init__(self, Lambda, lower, upper, T: LinearMap = None, P: LinearMap = None):
+        super().__init__(T, P)
         self.l = lower
         self.u = upper
         self.Lambda = float(Lambda)
 
 
-    def _apply(self, v, alpha):
+    def _apply(self, v: torch.Tensor, alpha: FloatLike):
         if self.P is None:
             x = torch.clamp(v, self.l, self.u)
         else:
@@ -220,3 +224,27 @@ class BoxConstraint(Prox):
             x = torch.minimum(u, torch.minimum(v, l))
         return x
 
+class Conj(Prox):
+    r"""
+    Proximal operator for convex conjugate function.
+
+    ..math::
+        Prox_{\alpha f^*}(v) = v - \alpha Prox_{frac{1}{\alpha} f}(\frac{1}{\alpha} v)
+    
+    Args:
+        prox (Prox): Proximal operator function
+    """
+
+    def __init__(self, prox: Prox):
+        self.prox = prox
+
+    def _apply(self, v, alpha):
+        assert alpha > 0, "alpha should be greater than 0"
+        return v - alpha * self.prox(v, 1/alpha)
+
+class Stack(Prox):
+    r"""
+    Stack proximal operators.
+
+
+    """
