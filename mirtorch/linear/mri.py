5084f1d70daa0d0b682091d726a25de0056e9ccc
m5520 <guanhuaw@umich.edu>
2021-10-18 23:38:48 -0400

    FIx mri

--- mirtorch/linear/mri.py
+++ mirtorch/linear/mri.py
@@ -140,25 +140,29 @@ class NuSense(LinearMap):
                  traj: Tensor,
                  norm='ortho',
                  batchmode=True,
-                 numpoints: Union[int, Sequence[int]] = 6):
+                 numpoints: Union[int, Sequence[int]] = 6,
+                 grid_size: float = 2):
         self.smaps = smaps
         self.norm = norm
         self.traj = traj
         self.batchmode = batchmode
+        assert grid_size >= 1, "grid size should be greater than 1"
         if batchmode:
-            self.A = tkbn.KbNufft(im_size=tuple(smaps.shape[2:]), grid_size=tuple(np.array(smaps.shape[2:]) * 2),
+            self.grid_size = tuple(np.floor(np.array(smaps.shape[2:]) * grid_size).astype(int))
+            self.A = tkbn.KbNufft(im_size=tuple(smaps.shape[2:]), grid_size=self.grid_size,
                                   numpoints=numpoints).to(smaps)
             self.AT = tkbn.KbNufftAdjoint(im_size=tuple(smaps.shape[2:]),
-                                          grid_size=tuple(np.array(smaps.shape[2:]) * 2),
+                                          grid_size=self.grid_size,
                                           numpoints=numpoints).to(smaps)
             size_in = [smaps.shape[0]] + [1] + list(smaps.shape[2:])
             size_out = list(smaps.shape[0:2]) + [traj.shape[-1]]
             super(NuSense, self).__init__(tuple(size_in), tuple(size_out), device=smaps.device)
         else:
-            self.A = tkbn.KbNufft(im_size=tuple(smaps.shape[1:]), grid_size=tuple(np.array(smaps.shape[1:]) * 2),
+            self.grid_size = tuple(np.floor(np.array(smaps.shape[1:]) * grid_size).astype(int))
+            self.A = tkbn.KbNufft(im_size=tuple(smaps.shape[1:]), grid_size=self.grid_size,
                                   numpoints=numpoints).to(smaps)
             self.AT = tkbn.KbNufftAdjoint(im_size=tuple(smaps.shape[1:]),
-                                          grid_size=tuple(np.array(smaps.shape[1:]) * 2),
+                                          grid_size=self.grid_size,
                                           numpoints=numpoints).to(smaps)
             size_in = smaps.shape[1:]
             size_out = [smaps.shape[0]] + [traj.shape[-1]]
@@ -168,13 +172,15 @@ class NuSense(LinearMap):
         if self.batchmode:
             return self.A(x, self.traj, smaps=self.smaps, norm=self.norm)
         else:
-            return self.A(x.unsqueeze(0).unsqueeze(0), self.traj, smaps=self.smaps.unsqueeze(0), norm=self.norm).squeeze(0).squeeze(0)
+            return self.A(x.unsqueeze(0).unsqueeze(0), self.traj, smaps=self.smaps.unsqueeze(0),
+                          norm=self.norm).squeeze(0).squeeze(0)
 
     def _apply_adjoint(self, y: Tensor) -> Tensor:
         if self.batchmode:
             return self.AT(y, self.traj, smaps=self.smaps, norm=self.norm)
         else:
             return self.AT(y.unsqueeze(0), self.traj, smaps=self.smaps.unsqueeze(0), norm=self.norm).squeeze(0)
+
 
 class NuSenseFrame(LinearMap):
     '''
@@ -198,33 +204,40 @@ class NuSenseFrame(LinearMap):
                  traj: Tensor,
                  norm='ortho',
                  batchmode=True,
-                 numpoints: Union[int, Sequence[int]] = 6):
+                 numpoints: Union[int, Sequence[int]] = 6,
+                 grid_size: float = 2):
         self.smaps = smaps
         self.norm = norm
         self.traj = traj
         self.batchmode = batchmode
         self.toep_op = tkbn.ToepNufft()
+
         if batchmode:
-            self.kernel = tkbn.calc_toeplitz_kernel(traj, [smaps.shape[0]] + list(smaps.shape[2:]))
+            self.grid_size = tuple(np.floor(np.array(smaps.shape[2:]) * grid_size).astype(int))
+            self.kernel = tkbn.calc_toeplitz_kernel(traj, list(smaps.shape[2:]),
+                                                    grid_size=self.grid_size, numpoints=numpoints, norm = self.norm)
             size_in = [smaps.shape[0]] + [1] + list(smaps.shape[2:])
             super(NuSenseFrame, self).__init__(tuple(size_in), tuple(size_in), device=smaps.device)
         else:
-            self.kernel = tkbn.calc_toeplitz_kernel(traj, list(smaps.shape[1:]))
+            self.grid_size = tuple(np.floor(np.array(smaps.shape[1:]) * grid_size).astype(int))
+            self.kernel = tkbn.calc_toeplitz_kernel(traj, list(smaps.shape[1:]), grid_size=self.grid_size,
+                                                    numpoints=numpoints, norm = self.norm)
             size_in = list(smaps.shape[1:])
             super(NuSenseFrame, self).__init__(tuple(size_in), tuple(size_in), device=smaps.device)
 
     def _apply(self, x: Tensor) -> Tensor:
         if self.batchmode:
-            return self.toep_op(x, self.kernel, smaps = self.smaps)
+            return self.toep_op(x, self.kernel, smaps=self.smaps)
         else:
-            return self.toep_op(x.unsqueeze(0).unsqueeze(0), self.kernel, smaps = self.smaps.unsqueeze(0)).squeeze(0).squeeze(0)
+            return self.toep_op(x.unsqueeze(0).unsqueeze(0), self.kernel, smaps=self.smaps.unsqueeze(0)).squeeze(
+                0).squeeze(0)
 
     def _apply_adjoint(self, y: Tensor) -> Tensor:
         if self.batchmode:
-            return self.toep_op(y, self.kernel, smaps = self.smaps)
+            return self.toep_op(y, self.kernel, smaps=self.smaps)
         else:
-            return self.toep_op(y.unsqueeze(0).unsqueeze(0), self.kernel, smaps = self.smaps.unsqueeze(0)).squeeze(0).squeeze(0)
-
+            return self.toep_op(y.unsqueeze(0).unsqueeze(0), self.kernel, smaps=self.smaps.unsqueeze(0)).squeeze(
+                0).squeeze(0)
 
 
 class Gmri(LinearMap):
